c79b7a68f2ca6473d4b20f44727cc3e8f43d3858
root <root@ip-172-31-44-48.us-east-2.compute.internal>
2020-03-07 14:19:22 +0000

    adding file in feature branch

--- index1.py
+++ index1.py
@@ -3,5 +3,6 @@ a=input("your name pls:")
 if name =='Avinash':
     break
 else:
+    print ("hi")
     print (a)
 
---
+++ index2.py
@@ -0,0 +1,5 @@
+a=int(input("pls enter age"))
+if a>18:
+    print ("elegible for voting")
+else:
+    print ("not elegible fr vote")
